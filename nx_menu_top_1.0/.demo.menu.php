f8632bd35801979d885e47c2c4ef024e624e28b6
imageprom <necris@imageprom.com>
2018-09-18 09:32:24 +0300

    Add horizontal multilevel template
    Horizontal multilevel template

---
+++ nx_menu_top_1.0/.demo.menu.php
@@ -0,0 +1,60 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Плитка", 
+		"/catalog", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+
+	Array(
+		"Мозаика", 
+		"/catalog", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+
+	Array(
+		"Ламинат", 
+		"/catalog", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+
+	Array(
+		"Пробка", 
+		"/catalog", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+
+	Array(
+		"Обои", 
+		"/catalog", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+
+	Array(
+		"Свет", 
+		"/catalog/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+
+	Array(
+		"Сантехника", 
+		"/catalog", 
+		Array(), 
+		Array("noindex"=>"1", "nofollow"=>"1", "class"=>"test"), 
+		"" 
+	),
+	
+);
+?>
