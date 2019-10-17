229363a21ab612e25c425dac70aff1ea7620d31b
imageprom <necris@imageprom.com>
2019-10-17 18:03:34 +0300

    Добавлена микроразметка

--- nx_menu_top_1.0/template.php
+++ nx_menu_top_1.0/template.php
@@ -1,7 +1,7 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?if (!empty($arResult)):?>
-<nav class="nx-up-menu-outer">
-<ul class="nx-up-menu">
+<nav class="nx-up-menu-outer" itemscope itemtype="https://www.schema.org/SiteNavigationElement">
+<ul class="nx-up-menu" itemscope itemtype="http://schema.org/ItemList">
 <?$previousLevel = 0;
 foreach($arResult as $arItem):?>
 	<?if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
@@ -9,30 +9,36 @@ foreach($arResult as $arItem):?>
 	<?endif?>
 	<?if ($arItem["IS_PARENT"]):?>
 	<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-	<li class="root parent-root<?if($arItem["SELECTED"]):?> selected<?endif?><?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>" >
+	<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ItemList" 
+		class="root parent-root<?if($arItem["SELECTED"]):?> selected<?endif?><?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>" 
+	>
 		<?if($arItem["PARAMS"]["noindex"]):?><!--noindex--><?endif?>
-		<a href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a>
+		<meta itemprop="name" content="<?=$arItem["TEXT"]?>" />
+		<a itemprop="url" href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a>
 		<?if($arItem["PARAMS"]["noindex"]):?><!--/noindex--><?endif?>
 	<ul>
 	<?else:?>
-	<li <?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>>
+	<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ItemList" 
+		<?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>
+	>
 		<?if($arItem["PARAMS"]["noindex"]):?><!--noindex--><?endif?>
-		<a href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> class="parent<?if ($arItem["SELECTED"]):?> item-selected<?endif?>"><?=$arItem["TEXT"]?></a>
+		<meta itemprop="name" content="<?=$arItem["TEXT"]?>" />
+		<a itemprop="url" href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> class="parent<?if ($arItem["SELECTED"]):?> item-selected<?endif?>"><?=$arItem["TEXT"]?></a>
 		<?if($arItem["PARAMS"]["noindex"]):?><!--/noindex--><?endif?>
 	<ul>
 	<?endif?>
 	<?else:?>
 	<?if ($arItem["PERMISSION"] > "D"):?>
 	<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-	<li class="root <?if($arItem["SELECTED"]):?> selected<?endif?> <?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>" >
+	<li itemprop="itemListElement" class="root <?if($arItem["SELECTED"]):?> selected<?endif?> <?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>" >
 		<?if($arItem["PARAMS"]["noindex"]):?><!--noindex--><?endif?>
-		<a href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a>
+		<a itemprop="url" href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a>
 		<?if($arItem["PARAMS"]["noindex"]):?><!--/noindex--><?endif?>
 	</li>
 	<?else:?>
-	<li <?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>>
+	<li itemprop="itemListElement" <?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>>
 		<?if($arItem["PARAMS"]["noindex"]):?><!--noindex--><?endif?>
-		<a href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> <?if ($arItem["SELECTED"]):?> class="item-selected"<?endif?>><?=$arItem["TEXT"]?></a>
+		<a itemprop="url" href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> <?if ($arItem["SELECTED"]):?> class="item-selected"<?endif?>><?=$arItem["TEXT"]?></a>
 		<?if($arItem["PARAMS"]["noindex"]):?><!--/noindex--><?endif?>
 	</li>
 	<?endif?>
