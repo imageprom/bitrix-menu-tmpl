ca3aef5fe04413ace2759145a3ea47a67f4eacb9
necris <necris@imageprom.com>
2022-07-05 13:00:38 +0300

    menu

--- nx_menu_top_1.0/template.php
+++ nx_menu_top_1.0/template.php
@@ -4,51 +4,24 @@
 <ul class="nx-up-menu" itemscope itemtype="http://schema.org/ItemList">
 <?$previousLevel = 0;
 foreach($arResult as $arItem):?>
-	<?if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
-	<?=str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"]));?>
-	<?endif?>
-	<?if ($arItem["IS_PARENT"]):?>
-	<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-	<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ItemList" 
-		class="root parent-root<?if($arItem["SELECTED"]):?> selected<?endif?><?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>" 
-	>
-		<?if($arItem["PARAMS"]["noindex"]):?><!--noindex--><?endif?>
-		<meta itemprop="name" content="<?=$arItem["TEXT"]?>" />
-		<a itemprop="url" href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a>
-		<?if($arItem["PARAMS"]["noindex"]):?><!--/noindex--><?endif?>
-	<ul>
-	<?else:?>
-	<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ItemList" 
-		<?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>
-	>
-		<?if($arItem["PARAMS"]["noindex"]):?><!--noindex--><?endif?>
-		<meta itemprop="name" content="<?=$arItem["TEXT"]?>" />
-		<a itemprop="url" href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> class="parent<?if ($arItem["SELECTED"]):?> item-selected<?endif?>"><?=$arItem["TEXT"]?></a>
-		<?if($arItem["PARAMS"]["noindex"]):?><!--/noindex--><?endif?>
-	<ul>
-	<?endif?>
-	<?else:?>
-	<?if ($arItem["PERMISSION"] > "D"):?>
-	<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-	<li itemprop="itemListElement" class="root <?if($arItem["SELECTED"]):?> selected<?endif?> <?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>" >
-		<?if($arItem["PARAMS"]["noindex"]):?><!--noindex--><?endif?>
-		<a itemprop="url" href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a>
-		<?if($arItem["PARAMS"]["noindex"]):?><!--/noindex--><?endif?>
-	</li>
-	<?else:?>
-	<li itemprop="itemListElement" <?if($arItem["PARAMS"]["class"]):?> <?=$arItem["PARAMS"]["class"]?><?endif?>>
-		<?if($arItem["PARAMS"]["noindex"]):?><!--noindex--><?endif?>
-		<a itemprop="url" href="<?=$arItem["LINK"]?>" <?if($arItem["PARAMS"]["nofollow"]):?>rel="nofollow"<?endif?> <?if ($arItem["SELECTED"]):?> class="item-selected"<?endif?>><?=$arItem["TEXT"]?></a>
-		<?if($arItem["PARAMS"]["noindex"]):?><!--/noindex--><?endif?>
-	</li>
-	<?endif?>
-	<?endif?>
-	<?endif?>
-	<?$previousLevel = $arItem["DEPTH_LEVEL"];?>
+
+    <?if ($arItem['PERMISSION'] > 'D'):?>
+        <li temprop="itemListElement"
+            itemscope itemtype="http://schema.org/ItemList"
+            class="root <?if($arItem['SELECTED']):?> selected<?endif?><?if($arItem['PARAMS']['class']):?> <?=$arItem['PARAMS']['class']?><?endif?>"
+        >
+            <a itemprop="url" href="<?=$arItem['LINK']?>"
+               <?if($arItem['PARAMS']['target']):?>target="<?=$arItem['PARAMS']['target']?>"<?endif?>
+               <?if($arItem['PARAMS']['nofollow']):?>rel="nofollow"<?endif?>
+            >
+                <?if($arItem['PARAMS']['noindex']):?><!--noindex--><?endif?>
+                    <?=$arItem['TEXT']?>
+                <?if($arItem['PARAMS']['noindex']):?><!--/noindex--><?endif?>
+            </a>
+        </li>
+    <?endif?>
+
 <?endforeach?>
-<?if ($previousLevel > 1)://close last item tags?>
-<?=str_repeat("</ul></li>", ($previousLevel-1) );?>
-<?endif?>
 </ul>
 </nav>
 <?endif?>
